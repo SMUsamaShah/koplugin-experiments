66b8c77a46090257afbe70e7991cf29bc92adc8b
Muhammad Usama <lifeh2o@gmail.com>
2026-09-07 22:30:58 +0100

    Add Animation Lab metadata

---
+++ animationlab.koplugin/_meta.lua
@@ -0,0 +1,7 @@
+local _ = require("gettext")
+return {
+    name = "animationlab",
+    fullname = _("E-Ink Animation Lab"),
+    version = "0.1.0",
+    description = _([[Benchmark fast E-Ink refresh modes and preview page-turn animations.]]),
+}
