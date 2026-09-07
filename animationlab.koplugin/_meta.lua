7663253b7f34d53f6605e14e943d19dfbd1f3c0e
Muhammad Usama <lifeh2o@gmail.com>
2026-09-07 22:51:32 +0100

    Bump Animation Lab to 0.2.0

--- animationlab.koplugin/_meta.lua
+++ animationlab.koplugin/_meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 return {
     name = "animationlab",
     fullname = _("E-Ink Animation Lab"),
-    version = "0.1.0",
-    description = _([[Benchmark fast E-Ink refresh modes and preview page-turn animations.]]),
+    version = "0.2.0",
+    description = _([[Benchmark fast E-Ink refresh modes and preview page-turn animations on the actual book pages being read.]]),
 }
